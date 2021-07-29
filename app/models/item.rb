7c70b32eddd2b02da95efd9cb6b1e7e7fc0b0024
pukutan2021 <86871601+pukutan2021@users.noreply.github.com>
2021-07-29 13:34:49 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,5 +9,13 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :category_id, numericality: { , message: "can't be blank"}
+  validates :name, presence: true
+  validates :info, presence: true
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :sales_status_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :shipping_fee_status_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :scheduled_delivery_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :price, presence: true
+ 
 end
