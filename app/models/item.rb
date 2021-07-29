366cbf61340da13fb98ffb7438365330b251c389
pukutan2021 <86871601+pukutan2021@users.noreply.github.com>
2021-07-29 13:03:13 +0900

    ActiveHashを導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,13 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :sales_status
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
+
   belongs_to :user
   has_one_attached :image
+
+  validates :category_id, numericality: { , message: "can't be blank"}
 end
