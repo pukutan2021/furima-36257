ad820270c5cea0e0336f7dc3a8721ee7ae112149
pukutan2021 <86871601+pukutan2021@users.noreply.github.com>
2021-07-30 17:52:00 +0900

    画像のバリデーションを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,6 +9,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  validates :image, presence: true
   validates :name, presence: true
   validates :info, presence: true
   validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
