7e85c9578b615a705ea8701f4ebd90479bcd2223
pukutan2021 <86871601+pukutan2021@users.noreply.github.com>
2021-08-01 20:09:15 +0900

    レビューによる修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:edit, :update, :destroy]
-  before_action :authenticate_user!, only: [:new, :create, :edit]
-  before_action :contributor_confirmation, only: [:edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
