a0f80126eba3815111c82031c43e578128d9fbb5
pukutan2021 <86871601+pukutan2021@users.noreply.github.com>
2021-08-02 14:12:28 +0900

    フォームの情報を保存できるようにする

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,6 +4,18 @@ class OrdersController < ApplicationController
   end
 
   def create
-    
+    @order_transaction = OrderTransaction.new(orders_params)
+    if @order_transaction.valid?
+      @order_transaction.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def orders_params
+    params.require(:order_transaction).permit(:postal_code, :prefecture_id, :city, :addresses, 
+      :building, :phone_number).merge(user_id: current_user.id)
   end
 end
--- app/models/order_transaction.rb
+++ app/models/order_transaction.rb
@@ -7,7 +7,7 @@ class OrderTransaction
     validates :user_id
     validates :item_id
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid"}
-    validates :prefecture_id, numericalith: {other_than: 1, message: "can't be blank"}
+    validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
     validates :city
     validates :addresses
     validates :phone_number
